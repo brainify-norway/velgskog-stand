d2bf7e8138f1819c5070956efc4798f236faa022
Kasper Aamodt <kasper@aamodt.xyz>
2022-10-06 09:29:13 +0200

    custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html lang="no">
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://use.typekit.net/dcw3wug.css"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
