20f6263e5d0e2ce4d70158b1c0b012a83a49c3bf
Kasper Aamodt <kasper@aamodt.xyz>
2023-02-23 21:22:37 +0100

    Menu

--- components/navMenu.tsx
+++ components/navMenu.tsx
@@ -1,7 +1,19 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { useState } from 'react';
 import { Collapse, Container, Nav, Navbar } from 'react-bootstrap';
+
+const menu = [
+    { label: 'Forsiden', href: 'https://velgskog.no/' },
+    { label: 'Studiesteder', href: 'https://velgskog.no/studiesteder/' },
+    { label: 'Jobbmuligheter', href: 'https://velgskog.no/jobbmuligheter/' },
+    {
+        label: 'Ledige stillinger',
+        href: 'https://velgskog.no/ledige-stillinger/',
+    },
+    { label: 'Om Velg Skog', href: 'https://velgskog.no/om-velg-skog/' },
+    { label: 'Ressursside', href: 'https://velgskog.no/ressursside/' },
+    { label: 'Nettbutikk', href: 'https://www.supportershoppen.no/velg-skog/' },
+];
 
 const NavMenu = () => {
     const [open, setOpen] = useState(false);
@@ -29,12 +41,15 @@ const NavMenu = () => {
                     ></div>
                     <Collapse className={open ? 'show' : 'hide'}>
                         <Nav id="nav-menu">
-                            <Link
-                                className="menu-link"
-                                href="https://velgskog.no/"
-                            >
-                                Forsiden
-                            </Link>
+                            {menu.map((item, index) => (
+                                <a
+                                    key={index}
+                                    className="menu-link"
+                                    href={item.href}
+                                >
+                                    {item.label}
+                                </a>
+                            ))}
                         </Nav>
                     </Collapse>
                 </Container>
