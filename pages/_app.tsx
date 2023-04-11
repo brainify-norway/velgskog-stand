c11ffc04c024ccb51b85f787a48db4c627d745bf
Kasper Aamodt <kasper@aamodt.xyz>
2023-04-11 12:11:29 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
 import '../styles/styles.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
-
-export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
     return (
@@ -8,6 +9,18 @@ export default function Document() {
                     rel="stylesheet"
                     href="https://use.typekit.net/dcw3wug.css"
                 />
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-09DB7WV1E1"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-09DB7WV1E1');
+                    `}
+                </Script>
             </Head>
             <body>
                 <Main />
